8090bbdef711f9f511bd983d682f23f681baa014
Kana Moore <dudiw.kana@hotmail.co.jp>
2020-04-07 19:38:38 -0500

    Completed basic part of hw

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,26 @@
-var myMap = L.map("map", {
-  center: [34.0522, -118.2437],
-  zoom: 6,
-});
-
-L.tileLayer(
-  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY,
-  }
-).addTo(myMap);
-
 var earthquakeData =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Define a markerSize function that will give each city a different radius based on its population
 function markerSize(mag) {
-  return mag * 10000;
+  return mag * 15000;
 }
+
+function getColor(d) {
+  return d === "0-1"
+    ? "rgba(183,243,77)"
+    : d === "1-2"
+    ? "rgba(225,243,77)"
+    : d === "2-3"
+    ? "rgba(243,219,77)"
+    : d === "3-4"
+    ? "rgba(243,186,77)"
+    : d === "4-5"
+    ? "rgb(240,167,107)"
+    : "rgb(240,107,107)";
+}
+
+var earthquakeCircles = [];
 
 d3.json(earthquakeData, function (data) {
   console.log(data);
@@ -57,22 +57,96 @@ d3.json(earthquakeData, function (data) {
       var color = "rgb(183,243,77)";
     }
 
-    L.circle(location, {
-      fillOpacity: 0.75,
-      color: "black",
-      weight: 1,
-      fillColor: color,
-      radius: markerSize(data.features[i].properties.mag),
-    })
-      .bindPopup(
-        "<h1>" +
-          data.features[i].properties.place +
-          "</h1>" +
-          "<hr>" +
-          "<h3>Magnitude: " +
-          data.features[i].properties.mag +
-          "</h3>"
-      )
-      .addTo(myMap);
+    earthquakeCircles.push(
+      L.circle(location, {
+        fillOpacity: 0.75,
+        color: "black",
+        weight: 1,
+        fillColor: color,
+        radius: markerSize(data.features[i].properties.mag),
+      })
+        .bindPopup(
+          "<h1>" +
+            data.features[i].properties.place +
+            "</h1>" +
+            "<hr>" +
+            "<h3>Magnitude: " +
+            data.features[i].properties.mag +
+            "</h3>"
+        )
+
+        .addTo(myMap)
+    );
   }
+
+  // Add a legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "info legend");
+    labels = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];
+
+    for (var i = 0; i < labels.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' +
+        getColor(labels[i]) +
+        '"></i> ' +
+        labels[i] +
+        (labels[i] ? "<br>" : "");
+    }
+    return div;
+  };
+  legend.addTo(myMap);
 });
+
+var satellite = L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.streets-satellite",
+    accessToken: API_KEY,
+  }
+);
+
+var streets = L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY,
+  }
+);
+
+var outdoors = L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: API_KEY,
+  }
+);
+
+var baseMaps = {
+  Street: streets,
+  Satellite: satellite,
+  Outdoors: outdoors,
+};
+
+var earthquakeLayer = L.layerGroup(earthquakeCircles);
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  Earthquake: earthquakeLayer,
+};
+
+var myMap = L.map("map", {
+  center: [34.0522, -118.2437],
+  zoom: 6,
+  layers: [streets, earthquakeLayer],
+});
+
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
